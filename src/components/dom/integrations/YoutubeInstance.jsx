03b70e54867baf191fd7d9d1a1bd7c096ea9427d
luxumbra <me@luxumbra.dev>
2022-06-15 15:32:43 +0100

    had the wrong stream url. fixed now.

--- src/components/dom/integrations/YoutubeInstance.jsx
+++ src/components/dom/integrations/YoutubeInstance.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 
 
 function YoutubeInstance () {
-  const [youtubeRoom, setYoutubeRoom] = useState('UC6gdZ6Q7Fwfvn-Uu4QKDyhg')
+  const [youtubeRoom, setYoutubeRoom] = useState('XCRh2p13mbM')
 
 
   return (
@@ -10,7 +10,7 @@ function YoutubeInstance () {
       title='Youtube Livestream'
       width='100%'
       height='100%'
-      src={`https://www.youtube.com/embed/live_stream?channel=${youtubeRoom}&amp;autoplay=1`}
+      src={`https://www.youtube.com/embed/${youtubeRoom}`}
       frameBorder='0'
       allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
       allowFullScreen
